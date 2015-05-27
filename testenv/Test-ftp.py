c0f8269bea6239f5305018fee3f20e5b1e731d21
Satyam Zode <satyamzode@gmail.com>
2015-05-27 19:36:40 +0530

    tests: Add test case for Wget Download from FTP Server * testenv/Test-ftp.py: New File

---
+++ testenv/Test-ftp.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+from sys import exit
+from test.ftp_test import FTPTest
+from test.base_test import FTP
+from misc.wget_file import WgetFile
+import os
+
+"""
+    This test ensures that Wget can download files from FTP Server
+"""
+TEST_NAME = "FTP Downloads"
+########## File Definitions ######################################
+File1 = "Beyond the fog lies clarity"
+
+A_File = WgetFile ("File1", File1)
+
+WGET_OPTIONS = " -S "
+WGET_URLS = [["File1"]]
+
+Files = [[A_File, B_File]]
+
+Servers = [FTP]
+
+ExpectedReturnCode = 0
+ExpectedDownloadedFiles = [A_File]
+
+######### Pre and Post Test Hooks ################################
+pre_test = {
+    "ServerFiles"   : Files
+}
+test_options = {
+    "WgetCommands"  : WGET_OPTIONS,
+    "Urls"          : WGET_URLS
+}
+post_test = {
+    "ExpectedFiles"     : ExpectedDownloadedFiles,
+    "ExpectedRetCode"   : ExpectedReturnCode
+}
+
+err = FTPTest (
+                name=TEST_NAME,
+                pre_hook=pre_test,
+                test_params=test_options,
+                post_hooks=post_test,
+                protocols=Servers
+).begin ()
+
+exit (err)
+
