e7c230fb1f77bbbe043b60add4d1a1c165934514
Satyam Zode <satyamzode@gmail.com>
2015-06-24 19:32:53 +0530

    Add new methods to FTP Handler *testenv/server/ftp/ftp_server.py : Add ftp_SIZE and ftp_RETR *testenv/server/ftp/ftp_server.py : Add new class FileProducer and _FileReadWriteError

--- testenv/server/ftp/ftp_server.py
+++ testenv/server/ftp/ftp_server.py
@@ -4,6 +4,7 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from exc.server_error import ServerError, AuthError
 import threading
 import socket
+import sys
 
 class StoppableFTPServer(FTPServer):
     def server_conf(self, filelist, conf_dict):
@@ -20,8 +21,10 @@ class _FTPHandler(FTPHandler):
                 arg = "/"
             elif cmd == 'LIST':
                 arg = "/"
+            """
             else:
                 arg = "/"
+            """
 
         self.process_command(cmd,arg)
 
@@ -52,6 +55,73 @@ class _FTPHandler(FTPHandler):
         except AuthError as se:
             self.respond('Login failed')
             raise se
+
+    def ftp_SIZE(self, path):
+        """Method to return size of file to the client"""
+        if self._current_type == 'a':
+            why = "SIZE not allowed in ASCII mode"
+            self.respond("550 %s." % why)
+            return
+
+        req_file = self.server.fileSys[path]
+        size = len(req_file)
+        self.respond('213 %s' % size)
+
+
+    def ftp_RETR(self, file):
+        """Method to retrive the file."""
+        rest_pos = self._restart_position
+        self._restart_position = 0
+
+        """We have requested file name. So retriving the contents of file name"""
+
+        file_contents = self.server.fileSys[file]
+
+
+        """Writing all the contents to the file."""
+
+        f = open("File1" , 'w')
+        f.write(file_contents)
+
+        fd = open("File1" , 'r')
+
+        producer = FileProducer(fd, self._current_type)
+        self.push_dtp_data(producer, isproducer=True, file=fd, cmd="RETR")
+        return file
+
+class _FileReadWriteError(OSError):
+    """Exception raised when reading or writing a file during a transfer."""
+
+class FileProducer(object):
+    """Producer wrapper for file[-like] objects."""
+
+    buffer_size = 65536
+
+    def __init__(self, file, type):
+        """Initialize the producer with a data_wrapper appropriate to TYPE.
+
+         - (file) file: the file[-like] object.
+         - (str) type: the current TYPE, 'a' (ASCII) or 'i' (binary).
+        """
+        self.file = file
+        self.type = type
+        if type == 'a' and os.linesep != '\r\n':
+            self._data_wrapper = lambda x: x.replace(b(os.linesep), b('\r\n'))
+        else:
+            self._data_wrapper = None
+
+    def more(self):
+        """Attempt a chunk of data of size self.buffer_size."""
+        try:
+            data = self.file.read(self.buffer_size)
+        except OSError:
+            err = sys.exc_info()[1]
+            raise _FileReadWriteError(err)
+        else:
+            if self._data_wrapper is not None:
+                data = self._data_wrapper(data)
+            return data
+
 
 class FTPd(threading.Thread):
     server_class = StoppableFTPServer
